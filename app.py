fbec8dd18ad8704411cc69d42be49dfde1bb52d0
adnin-alzuhri-siregar <adninalzuhrisiregar@gmail.com>
2026-07-14 23:04:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,8 @@ with tab_prediksi:
     try:
         ai_model = joblib.load('mesin_prediksi_sales_v1.pkl')
         model_siap = True
-        except Exception as e:
-    st.error(f"⚠️ Error memuat model: {e}")
+    except Exception as e:
+        st.error(f"⚠️ Error memuat model: {e}")
         model_siap = False
 
     if model_siap:
